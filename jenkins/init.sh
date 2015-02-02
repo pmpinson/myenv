3dde9cb9df03640cffc5b07290a79201f40867a7
pmpinson <pmpinson@users.noreply.github.com>
2015-02-02 08:28:36 -0500

    Update init.sh

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-sudo -i
-
-
 # mount jenkins-backup volume
-mount /dev/xvdb /data
-cp /etc/fstab /etc/fstab.origin
-(cat /etc/fstab ; echo "/dev/xvdb       /data   ext4    defaults,nofail 0   0") > /etc/fstab
+sudo mount /dev/xvdb /data
+sudo cp /etc/fstab /etc/fstab.origin
+sudo (cat /etc/fstab ; echo "/dev/xvdb       /data   ext4    defaults,nofail 0   0") > /etc/fstab
 
 # install docker
-yum install -y docker
-service docker start
+sudo yum install -y docker
+sudo service docker start
 
 # build jenkins image
-wget https://raw.githubusercontent.com/pmpinson/dockerfile/master/jenkins/1.580.2/Dockerfile
-docker build --tag=pmpinson/jenkins:1.580.2 . && docker build --tag=pmpinson/jenkins .
+sudo wget https://raw.githubusercontent.com/pmpinson/dockerfile/master/jenkins/1.580.2/Dockerfile
+sudo docker build --tag=pmpinson/jenkins:1.580.2 . && docker build --tag=pmpinson/jenkins .
 
 # launch jenkins
-docker run -d --name dev-jenkins-server -p 9004:8080 -v /data/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock pmpinson/jenkins
-
-exit
+sudo docker run -d --name dev-jenkins-server -p 9004:8080 -v /data/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock pmpinson/jenkins
